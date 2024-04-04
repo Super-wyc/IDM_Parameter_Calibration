c194501fc3514d73a231eddb626027b82eb17f79
Stalemate <1094552086@qq.com>
2024-04-04 16:21:00 +0800

    IDMsimulate更新

--- IDMsimulate.py
+++ IDMsimulate.py
@@ -26,11 +26,14 @@ class IDM():
                      leader_position:pd.Series,       # 引导车位置列表
                      follower_length:float,      # 跟驰车车长
                      follower_initial_velocity:float,    # 跟驰车初始速度
-                     follower_initial_position:float     # 跟驰车初始位置
-                     
-                     )-> list:
+                     follower_initial_position:float,     # 跟驰车初始位置
+                     lane:int=2
+                     )-> pd.DataFrame:
         
-        min_s=leader_length       # 恰好不相撞距离
+        if lane<=2:
+            min_s=leader_length       # 恰好不相撞距离
+        else:
+            min_s=follower_length
 
         # 初始化跟驰车
         follower_position=np.zeros(leader_position)
@@ -40,7 +43,7 @@ class IDM():
 
         # 模拟
         for i in range(1,len(leader_position)):
-            delta_v = follower_velocity[i-1] - leader_velocity[i-1]
+            delta_v = leader_velocity[i-1]-follower_velocity[i-1]
             s_star = IDM.s0 + max(0, follower_velocity[i-1]*IDM.T + (follower_velocity[i-1]*delta_v)/(2*np.sqrt(IDM.max_acceleration*IDM.b)))
             s = leader_position[i-1] - follower_position[i-1] - min_s
             acceleration = IDM.max_acceleration * (1 - (follower_velocity[i-1]/IDM.desired_velocity)**IDM.delta - (s_star/s)**2)
